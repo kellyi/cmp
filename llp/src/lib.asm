4dcd52c84ef57fe4c6d3734874a9925dfc903117
Kelly Innes <kinnes@gmail.com>
2018-05-13 00:30:08 -0400

    Add string_length function

--- llp/src/lib.asm
+++ llp/src/lib.asm
@@ -1,13 +1,28 @@
 global _start
 
+section .data
+test_string: db "hello!", 0
+
 section .text
 
 exit:
   mov rax, 60                   ; set up the exit instruction
   syscall
 
+string_length:
+.loop:
+  cmp byte [rdi+r13], 0
+  je .end
+  inc r13
+  jmp .loop
+.end:
+  mov rax, r13
+  ret
+
 _start:
-  mov rdi, 10
+  mov rdi, test_string
+  call string_length
+  mov rdi, rax
   call exit
 
 
