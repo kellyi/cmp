c122c5ce4b26b7d99f326a40000d7e27d67414ab
Kelly Innes <kinnes@gmail.com>
2018-05-15 23:37:29 -0400

    Division

---
+++ llp/src/division.asm
@@ -0,0 +1,20 @@
+%include 'functions.asm'
+
+SECTION .data
+message db ' remainder '
+
+SECTION .text
+global _start
+
+_start:
+
+  mov eax, 90
+  mov ebx, 9
+  div ebx
+  call iprint
+  mov eax, message
+  call sprint
+  mov eax, edx
+  call iprintlf
+
+  call quit
