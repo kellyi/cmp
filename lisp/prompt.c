d3b50990bbb33e1f0c574468b7ae8807509d21f9
Kelly Innes <kinnes@gmail.com>
2018-06-29 18:43:29 -0400

    Evaluate q-expressions

--- lisp/prompt.c
+++ lisp/prompt.c
@@ -1,6 +1,9 @@
 #include "mpc.h"
 #include <editline/readline.h>
 
+#define LASSERT(args, cond, err) \
+  if (!(cond)) { lval_del(args); return lval_err(err); }
+
 enum { LVAL_ERR, LVAL_NUM, LVAL_SYM, LVAL_SXPR, LVAL_QXPR };
 
 typedef struct lval {
@@ -119,6 +122,77 @@ void lval_print(lval* v) {
 }
 
 void lval_println(lval* v) { lval_print(v); putchar('\n'); }
+lval* lval_eval(lval* v);
+
+lval* builtin_head(lval *a) {
+  LASSERT(a, a->count == 1,
+          "Function 'head' passed too many arguments!");
+  LASSERT(a, a->cell[0]->type == LVAL_QXPR,
+          "Function 'head' passed incorrect type!");
+  LASSERT(a, a->cell[0]->count != 0,
+          "Function 'head' passed {}!");
+
+  lval* v = lval_take(a, 0);
+  while (v->count > 1) { lval_del(lval_pop(v, 1)); }
+  return v;
+}
+
+lval* builtin_tail(lval* a) {
+  LASSERT(a, a->count == 1,
+          "Function 'tail' passed too many arguments!");
+  LASSERT(a, a->cell[0]->type == LVAL_QXPR,
+          "Function 'tail' passed incorrect type!");
+  LASSERT(a, a->cell[0]->count != 0,
+          "Function 'tail' passed {}!");
+
+  lval* v = lval_take(a, 0);
+  lval_del(lval_pop(v, 0));
+  return v;
+}
+
+lval* builtin_list(lval* a) {
+  a->type= LVAL_QXPR;
+  return a;
+}
+
+lval* builtin_eval(lval* a) {
+  LASSERT(a, a->count == 1,
+          "Function 'eval' passed too many arguments!");
+  LASSERT(a, a->cell[0]->type == LVAL_QXPR,
+          "Function 'eval' passed incorrect type!");
+
+  lval* x = lval_take(a, 0);
+  x->type = LVAL_SXPR;
+
+  return lval_eval(x);
+}
+
+lval* lval_join(lval* x, lval* y) {
+  while (y->count) {
+    x = lval_add(x, lval_pop(y, 0));
+  }
+
+  lval_del(y);
+
+  return x;
+}
+
+lval* builtin_join(lval* a) {
+  for (int i = 0; i < a->count; i++) {
+    LASSERT(a, a->cell[i]->type == LVAL_QXPR,
+            "Function 'join' passed incorrect type!")
+  }
+
+  lval* x = lval_pop(a, 0);
+
+  while (a->count) {
+    x = lval_join(x, lval_pop(a, 0));
+  }
+
+  lval_del(a);
+
+  return x;
+}
 
 lval* builtin_op(lval* a, char* op) {
   for (int i = 0; i < a->count; i++) {
@@ -154,7 +228,18 @@ lval* builtin_op(lval* a, char* op) {
   return x;
 }
 
-lval* lval_eval(lval* v);
+lval* builtin(lval* a, char* func) {
+  if (strcmp("list", func) == 0) { return builtin_list(a); }
+  if (strcmp("head", func) == 0) { return builtin_head(a); }
+  if (strcmp("tail", func) == 0) { return builtin_tail(a); }
+  if (strcmp("join", func) == 0) { return builtin_join(a); }
+  if (strcmp("eval", func) == 0) { return builtin_eval(a); }
+  if (strstr("+-/*%", func)) { return builtin_op(a, func); }
+
+  lval_del(a);
+
+  return lval_err("Unknown function!");
+}
 
 lval* lval_eval_sxpr(lval* v) {
   for (int i = 0; i < v->count; i++) {
@@ -172,7 +257,7 @@ lval* lval_eval_sxpr(lval* v) {
     lval_del(f); lval_del(v);
     return lval_err("S-expression Does not start with symbol.");
   }
-  lval* result = builtin_op(v, f->sym);
+  lval* result = builtin(v, f->sym);
   lval_del(f);
 
   return result;
@@ -222,7 +307,9 @@ int main(int argc, char** argv) {
   mpca_lang(MPCA_LANG_DEFAULT,
     "                                          \
       number : /-?[0-9]+/ ;                    \
-      symbol : '+' | '-' | '*' | '/' | '%';    \
+      symbol : '+' | '-' | '*' | '/' | '%'     \
+             | \"list\" | \"head\" | \"tail\"  \
+             | \"join\" | \"eval\";            \
       sxpr  : '(' <expr>* ')' ;                \
       qxpr  : '{' <expr>* '}' ;                \
       expr   : <number> | <symbol> | <sxpr> | <qxpr> ;  \
